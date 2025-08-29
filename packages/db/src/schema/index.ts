e9464889cd577461c6f8487d17d63456bb1f84d6
mavix21 <marcevizcarra2109@hotmail.com>
2025-08-29 17:34:21 -0500

    remove: unused db package

--- packages/db/src/schema/index.ts
+++
@@ -1,5 +0,0 @@
-export { users, type SelectUser, type InsertUser } from "./users";
-export { account } from "./account";
-export { session } from "./session";
-export { verification } from "./verification";
-export { jwks } from "./jwks";
