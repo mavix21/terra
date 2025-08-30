9e46c861e474cbab10debe22c988ee1f4315ae58
mavix21 <marcevizcarra2109@hotmail.com>
2025-08-30 00:38:41 -0500

    fix: update BASE_URL to use NEXT_PUBLIC_URL for production environment

--- apps/web/lib/constants.ts
+++ apps/web/lib/constants.ts
@@ -3,5 +3,5 @@ import { env } from "@/env";
 export const BASE_URL =
   env.NODE_ENV === "production"
     ? // eslint-disable-next-line no-restricted-properties
-      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      `https://${process.env.NEXT_PUBLIC_URL}`
     : "http://localhost:3000";
