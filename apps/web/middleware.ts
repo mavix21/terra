428802d68966bf6f4dff101bc8fabb6f47886343
mavix21 <marcevizcarra2109@hotmail.com>
2025-08-31 05:36:32 -0500

    fix: correct redirection URL in auth middleware to ensure proper navigation

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -15,7 +15,7 @@ const authMiddleware = auth((req) => {
   const reqUrl = new URL(req.url);
 
   if (reqUrl.pathname !== "/") {
-    return NextResponse.redirect(new URL(`${BASE_URL}/auth/sign-in}`, req.url));
+    return NextResponse.redirect(new URL(`${BASE_URL}/auth/sign-in`, req.url));
   }
 });
 
